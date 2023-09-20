2f68f521a5953589ca82aeab962ab1d88ca66a2e
MrAsieru <28875845+MrAsieru@users.noreply.github.com>
2023-09-20 13:01:46 +0200

    commit inicial

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'tfg.web',
+  appName: 'tfg-web',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
