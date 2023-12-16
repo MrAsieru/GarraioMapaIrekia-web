c9761557e7502a0828b37878d6548b6f880818ef
MrAsieru <28875845+MrAsieru@users.noreply.github.com>
2023-12-16 14:16:14 +0100

    fix: icono tipo linea y puertos pred.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: "http://localhost/api",
-  websocketBaseUrl: 'ws://localhost/api',
-  tilesUrl: 'http://localhost/tile'
+  apiBaseUrl: "http://localhost:81/api",
+  websocketBaseUrl: 'ws://localhost:81/api',
+  tilesUrl: 'http://localhost:81/tile'
 };
